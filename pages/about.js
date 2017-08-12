53364cc05c48e1fa6f8dba15496adaf83b00c3b2
Adam Johnston <adam.johnston3679@gmail.com>
2017-08-12 13:33:35 +0100

    link index to about page

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function() {
+  return (
+    <div>
+      <p>
+        An about page
+      </p>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+import Link from 'next/link'
+
 export default function() {
   return (
     <div>
+      <Link
+        href="/about">
+        <button>
+          About
+        </button>
+      </Link>
+
       <p>
         Hello Next.js
       </p>
