62b164bff189652dde9a18ca064b4730daf2d8a8
Adam Johnston <adam.johnston3679@gmail.com>
2017-08-12 18:36:49 +0100

    styling

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,58 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 import fetch from 'isomorphic-unfetch'
 
+const getPosts = () => ([
+  { id: 'hello-next-js', title: 'Hello Next.js' },
+  { id: 'learn-next-js', title: 'Learn Next.js is Awesome' },
+  { id: 'deploy-next-js', title: 'Deploy Apps with Zeit' },
+])
+
+const PostLink = ({ id, title }) => (
+  <li>
+    <Link
+      as={`/post/${id}`}
+      href={`/post?title=${title}`}>
+      <a>{title}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
+)
+
 const Index = (props) => (
   <Layout>
     <h1>My Blog</h1>
 
     <ul>
-      {props.shows.map(({ show: { id, name } }) => (
-        <li key={id}>
-          <Link
-            as={`/post/${id}`}
-            href={`/post?id=${id}`}>
-            <a>{name}</a>
-          </Link>
-        </li>
+      {getPosts().map((post) => (
+        <PostLink
+          key={post.id}
+          {...post} />
       ))}
     </ul>
+
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+    `}</style>
   </Layout>
 )
 
